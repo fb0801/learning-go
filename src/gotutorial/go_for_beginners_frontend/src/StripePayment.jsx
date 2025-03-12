e3bc1fb74928a499d2e07b75e5fda00f505a9e39
fb0801 <fbhatti0801@gmail.com>
2025-03-12 17:04:51 +0000

    project setup 1.5

--- src/gotutorial/go_for_beginners_frontend/src/StripePayment.jsx
+++ src/gotutorial/go_for_beginners_frontend/src/StripePayment.jsx
@@ -9,7 +9,7 @@ import "./StripePayment.css";
 // recreating the Stripe object on every render.
 // This is your test publishable API key.
 const stripePromise = loadStripe(
-  "pk_test_51N9TTzAx5un0U0mJbl9Ha8mi1TdVPRd4WgGSk5pGPfZEFqKoIlksgaznEOQ25cLCbRMfiU9osCcGQTCY1m4BKXp600xvQ3uQCz"
+  ""
 );
 
 export default function StripePayment({ customerData }) {
