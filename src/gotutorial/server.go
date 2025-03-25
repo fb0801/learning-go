c879f255707368ba26a843c5d77997049f72cffd
fb0801 <fbhatti0801@gmail.com>
2025-03-25 17:58:02 +0000

    build backend 2.7

--- src/gotutorial/server.go
+++ src/gotutorial/server.go
@@ -6,7 +6,10 @@ import (
 	"net/http"
 )
 
+
 func main() {
+
+
 	http.HandleFunc("/create-payment-intent", handleCreatePaymentIntent)
     http.HandleFunc("/health", handleHealth)
     
@@ -22,6 +25,19 @@ func handleCreatePaymentIntent(writer http.ResponseWriter, request *http.Request
     if request.Method != "POST" {
         http.Error(writer, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
         return 
+    }
+
+    var req struct {
+        ProductId string `json:"product_id"`
+        FirstName string `json:"first_name"`
+        LastName string `json:"last_name"`
+        Address1 string `json:"address_1"`
+        Address2 string `json:"address_2"`
+        City string `json:"city"`
+        State string `json:"state"`
+        Zip string `json:"zip"`
+        Country string `json:"country"`
+
     }
 }
 
